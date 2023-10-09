5d4b595ed52d63bf81fd049ad425881696307c24
João Paulo <joao.paulomonteiro@hotmail.com>
2023-10-09 19:39:53 -0300

    first commit

---
+++ src/main/java/br/com/joaomonteiro/todolist/user/UserModel.java
@@ -0,0 +1,11 @@
+package br.com.joaomonteiro.todolist.user;
+
+import com.fasterxml.jackson.databind.util.internal.PrivateMaxEntriesMap;
+
+public class UserModel {
+  
+  private String username;
+  private String name;
+  private String password;
+
+}
